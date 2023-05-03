df63ef776599aeea2fc10a96aa78dba45caa2630
joris-landaret <joris.landaret@laplateforme.io>
2023-05-03 13:12:22 +0200

    création des classes user selon le MVC

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Controller\UserController;
+
 require_once 'vendor/autoload.php';
 
 $router = new AltoRouter();
@@ -18,30 +20,12 @@ $router->map('GET', '/users/[i:id]', function ($id) {
     echo 'Bienvenu sur la page de l\'utilisateur ' . $id;
 }, 'id-users');
 
-$router->map('GET', '/users/list', function () {
-    $array = [
-        ['email' => 'paul@logo.fr', 'first' => 'Paul', 'last' => 'Logo', 'password' => 'user'],
-        ['email' => 'marc@coco.com', 'first' => 'Marc', 'last' => 'Coco', 'password' => 'user'],
-        ['email' => 'sandy@dob.com', 'first' => 'Sandy', 'last' => 'Dob', 'password' => 'user'],
-        ['email' => 'adri@merc.fr', 'first' => 'Adri', 'last' => 'Merc', 'password' => 'user'],
-        ['email' => 'anny@marsy.fr', 'first' => 'Anny', 'last' => 'Marsy', 'password' => 'user']
-    ];
-
-    $db = new PDO('mysql:host=localhost;dbname=super-week;charset=utf8;port=3307', 'root', '');
-    var_dump($db);
-
-    $sql = "INSERT INTO `user` (`email`,`first_name`,`last_name`,`password`) 
-    VALUE (?,?,?,?)";
-    $request = $db->prepare($sql);
-
-    foreach ($array as $key) {
-
-        var_dump($key);
-        $request->execute(array($key['email'], $key['first'], $key['last'], password_hash($key['password'], PASSWORD_DEFAULT)));
-    }
+$router->map('GET', '/users/create', function () {
+    $userController = new UserController;
+    $userController->fillDB();
 }, 'users-bdd');
 
-$router->map('GET', '/book/list', function () {
+$router->map('GET', '/book/create', function () {
     $array2 = [
         ['title' => 'Ronron', 'content' => 'Ronflex', 'id_user' => 2],
         ['title' => 'Coco', 'content' => 'Musique', 'id_user' => 4],
@@ -52,15 +36,12 @@ $router->map('GET', '/book/list', function () {
     ];
 
     $db = new PDO('mysql:host=localhost;dbname=super-week;charset=utf8;port=3307', 'root', '');
-    var_dump($db);
 
     $sql = "INSERT INTO `book` (`title`,`content`,`id_user`) 
     VALUE (?,?,?)";
     $request = $db->prepare($sql);
 
     foreach ($array2 as $book) {
-
-        var_dump($book);
         $request->execute(array($book['title'], $book['content'], $book['id_user']));
     }
 }, 'book-bdd');
