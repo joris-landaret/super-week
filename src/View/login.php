8bed0a5adac18b0ddd51dcdf32bf49a2bf00d076
joris-landaret <joris.landaret@laplateforme.io>
2023-05-05 09:31:58 +0200

    create a login form

---
+++ src/View/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <label for="">Email</label>
+        <input type="email">
+        <label for="">Mot de pass</label>
+        <input type="password" name="password">
+        <input type="submit" value="Se connecter">
+    </form>
+
+</body>
+
+</html>
