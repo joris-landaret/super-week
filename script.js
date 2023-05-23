8faf09fe3b36736a58a9b2e1428dc6ce8da55a5d
joris-landaret <joris.landaret@laplateforme.io>
2023-05-23 11:38:13 +0200

    display one user

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const book = "/books";
 
 // Display Users
 const button = document.createElement("button");
-button.innerHTML = "afficher les utilisateurs ?";
+button.innerHTML = "Afficher les utilisateurs ?";
 document.body.append(button);
 
 const div = document.createElement("div");
@@ -17,4 +17,25 @@ button.addEventListener("click", async () => {
     div.innerHTML = "</br>"+text;
     console.log(div.innerHTML);
     
+});
+
+let input = document.createElement("input");
+input.setAttribute('type', 'text');
+input.setAttribute('placeholder', 'id de l\'utilisateur');
+document.body.append(input);
+let user_id = input.value;
+
+const submit = document.createElement("button");
+submit.innerText = "Envoyer"
+document.body.append(submit);
+
+submit.addEventListener("click", async () => {
+    
+    const user_id = input.value;
+    console.log(user_id);
+    const request = await fetch(user+"/"+user_id)
+    console.log(request);
+    const text = await request.text();
+    console.log(text);
+    div.innerHTML = "</br>"+text;
 });
